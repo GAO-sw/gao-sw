a1aadaae07f7a3b7245b17e0ea855a5d84a135ab
GAO-sw <gaosw@yandex.com>
2024-09-20 13:20:16 +0300

    added addition operation

--- program.py
+++ program.py
@@ -2,4 +2,5 @@ print("Hello,World!")
 a = int(input("Please input first integer: "))
 b = int(input("please input second integer: "))
 print(f"The integer you inputted is {a} 和 {b}")
-	
+sum = a + b
+print(f"The sum of two integers is : {sum}")	
